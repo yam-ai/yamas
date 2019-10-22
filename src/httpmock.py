b1dfae842c7a88896748fa19f2b0b0c3efa6cd4d
Thomas Lee <thomaslee@yam.ai>
2019-10-22 17:56:05 +0800

    Write bootstrap code

---
+++ src/httpmock.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os, sys
+from getopt import getopt, GetoptError
+
+DEFAULT_IP = '127.0.0.1'
+DEFAULT_PORT = 8000
+
+class HTTPMock(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)        
+        self.wfile.write('GET {}'.format(self.path).encode())
+        return
+
+    def do_POST(self):
+        self.send_response(200)
+        self.wfile.write('POST {}'.format(self.path).encode())
+        return
+
+def run(ip, port):
+    print('HTTP server is starting...')
+    server_address = (ip, port)
+    httpd = HTTPServer(server_address, HTTPMock)
+    print('HTTP server is running.')
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    try:
+        opts, args = getopt(sys.argv[1:], 'e', ['endpoint='])
+    except GetoptError as err:
+        print(err)
+        sys.exit(2)
+    ip, port = DEFAULT_IP, DEFAULT_PORT
+    for k, v in opts:
+        if k in ('e', '--endpoint'):
+            parts = v.split(':')
+            if len(parts) > 0:
+                ip = parts[0]
+            if len(parts) >= 1:
+                port = int(parts[1])
+    run(ip, port)
+
+
